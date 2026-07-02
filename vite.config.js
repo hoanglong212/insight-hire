f8f96ce8a9efa50e4e32887f8a6237bfeacf8afb
hoanglong212 <69975681+hoanglong212@users.noreply.github.com>
2026-07-02 10:15:32 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/cos30043/s10556692/A2/'
-})
+  base: process.env.VERCEL ? "/" : "/cos30043/s10556692/A2/",
+});
