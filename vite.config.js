ff5d346576db36a28c40798151b94693e3191859
hoanglong212 <69975681+hoanglong212@users.noreply.github.com>
2026-07-02 10:19:30 +0700

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+const basePath =
+  process.env.VITE_BASE_PATH ||
+  (process.env.VERCEL ? "/" : "/cos30043/s10556692/A2/");
+
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.VERCEL ? "/" : "/cos30043/s10556692/A2/",
+  base: basePath,
 });
